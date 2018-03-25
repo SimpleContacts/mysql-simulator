160c05eebffff436c0c66c70134a7be5ac0f1515
Vincent Driessen <me@nvie.com>
2018-03-25 22:37:37 +0200

    Make output format more consumable

--- src/runner.js
+++ src/runner.js
@@ -7,33 +7,36 @@ import { addTable, emptyDb, removeTable, renameTable } from './db';
 import type { Database, Table } from './types';
 
 // eslint-disable-next-line no-console
-const log = console.log
+const log = console.log;
 // eslint-disable-next-line no-console
-const error = console.error
+const error = console.error;
 
-function makeTable(expr): Table {
-  const fields = expr.definitions.filter(def => def.type === 'COLUMN');
-  // const fks = expr.definitions.filter(def => def.type === 'FOREIGN KEY');
-  // eslint-disable-next-line
+function makeTable(table): Table {
+  const columns = table.definitions.filter(def => def.type === 'COLUMN');
   return {
-    name: expr.tblName,
-    fields: fields.map(f => {
-      if (!f.attrs) {
-        // eslint-disable-next-line
-        console.log({ f });
-        throw new Error('???');
-      }
-
-      const nullable = !f.attrs.includes('NOT NULL');
-      const defaultValue = f.defaultValue;
+    name: table.tblName,
+    columns: columns.map(col => {
+      const nullable = !col.definition.nullable;
+      const defaultValue = col.definition.defaultValue;
       return {
-        name: f.name,
-        type: f.columnType,
+        name: col.colName,
+        type: col.definition.dataType,
         nullable,
         defaultValue,
       };
     }),
   };
+}
+
+function printDb(db: Database) {
+  for (const table of Object.values(db.tables)) {
+    log('');
+    log(chalk.blue(`${table.name}`));
+    log(chalk.blue('-'.repeat(table.name.length)));
+    for (const col of table.columns) {
+      log(`  ${chalk.magenta(col.name)} ${chalk.gray(col.type)}`);
+    }
+  }
 }
 
 function main() {
@@ -44,11 +47,11 @@ function main() {
     if (expr.type === 'CREATE TABLE') {
       const table = makeTable(expr);
       db = addTable(db, table);
-      log(chalk.green(`CREATE TABLE ${expr.name}`));
+      log(chalk.green(`CREATE TABLE ${expr.tblName}`));
     } else if (expr.type === 'DROP TABLE') {
       db = removeTable(db, expr.tableName, expr.ifExists);
     } else if (expr.type === 'RENAME TABLE') {
-      db = renameTable(db, expr.existingName, expr.newName);
+      db = renameTable(db, expr.tblName, expr.newName);
     } else {
       error(chalk.gray(`Unknown expression type: ${expr.type}`));
     }
@@ -58,7 +61,7 @@ function main() {
 
   log('');
   log('Done!');
-  log(chalk.blue(JSON.stringify(db, null, 2)));
+  printDb(db);
 }
 
 main();
