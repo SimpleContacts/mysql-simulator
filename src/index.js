881fdd31c1c63daa5813e0c049c3919bd12bbf18
Vincent Driessen <me@nvie.com>
2018-05-25 16:15:40 +0200

    Advertise data structures externally

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 // @flow
 
+import Column from './sim/Column';
 import Database from './sim/Database';
+import ForeignKey from './sim/ForeignKey';
+import Index from './sim/Index';
+import type { IndexType } from './sim/Index';
 import { applySql, applySqlFile, applySqlFiles, dumpDb, simulate } from './sim/lib';
+import Table from './sim/Table';
 
-export { Database, applySql, applySqlFile, applySqlFiles, dumpDb, simulate };
+export type { IndexType };
+export { applySql, applySqlFile, applySqlFiles, dumpDb, simulate, Column, Database, ForeignKey, Index, Table };
 export default simulate;
