6b8f514d71b6f313d29b253f1de4cfda7ee2d42b
Vincent Driessen <me@nvie.com>
2018-05-25 15:27:51 +0200

    Fix exports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 // @flow
 
-import Column from './sim/Column';
 import Database from './sim/Database';
-import ForeignKey from './sim/ForeignKey';
-import type { IndexType } from './sim/Index';
-import Index from './sim/Index';
 import { applySql, applySqlFile, applySqlFiles, dumpDb, simulate } from './sim/lib';
-import Table from './sim/Table';
 
-export type { Column, ForeignKey, Index, IndexType };
-
+export { Database, applySql, applySqlFile, applySqlFiles, dumpDb, simulate };
 export default simulate;
-export { applySql, applySqlFile, applySqlFiles, Database, dumpDb, simulate, Table };
