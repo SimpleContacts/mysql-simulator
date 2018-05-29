07fb9cae73b56a350e632be22708cb15bbde8922
Vincent Driessen <me@nvie.com>
2018-05-29 12:39:17 +0200

    Export data types at the package level

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@
 
 import Column from './sim/Column';
 import Database from './sim/Database';
+import type {
+  BinaryDataType,
+  DateTimeDataType,
+  EnumDataType,
+  IntDataType,
+  OtherDataType,
+  RealDataType,
+  TextDataType,
+  TypeInfo,
+} from './sim/DataType';
 import ForeignKey from './sim/ForeignKey';
 import Index from './sim/Index';
 import type { IndexType } from './sim/Index';
 import { applySql, applySqlFile, applySqlFiles, dumpDb, simulate } from './sim/lib';
 import Table from './sim/Table';
 
-export type { IndexType };
+export type {
+  BinaryDataType,
+  DateTimeDataType,
+  EnumDataType,
+  IndexType,
+  IntDataType,
+  OtherDataType,
+  RealDataType,
+  TextDataType,
+  TypeInfo,
+};
 export { applySql, applySqlFile, applySqlFiles, dumpDb, simulate, Column, Database, ForeignKey, Index, Table };
 export default simulate;
