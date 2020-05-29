d0cbb3f695fa6f258c5c39e1608ef81cf3523949
Vincent Driessen <me@nvie.com>
2020-05-29 17:12:11 +0200

    More cases

--- tests/36-generated-column-expression-identifiers.sql
+++ tests/36-generated-column-expression-identifiers.sql
@@ -24,6 +24,10 @@ CREATE TABLE t2 (
     b2 BOOLEAN AS (c1 IS NULL OR c2 IS NOT NULL),
     b3 BOOLEAN AS (c1 XOR c2),
     b4 BOOLEAN AS (COALESCE(c1, 'foo') XOR COALESCE(c2, 'bar')),
+    b5 BOOLEAN AS (true XOR false),
+    b6 BOOLEAN AS (1 XOR 0),
+    b7 BOOLEAN AS ('true' XOR 'false'),
+    b8 BOOLEAN AS (null XOR ''),
 
     x1 VARCHAR(20) AS (JSON_EXTRACT(c1, '$.type')),
     x2 VARCHAR(20) AS (c1 -> '$.type'),
