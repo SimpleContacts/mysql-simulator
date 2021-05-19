0bbf1a0ad0083a396ace4fa39dea594389f88275
Vincent Driessen <me@nvie.com>
2021-05-19 13:06:16 +0200

    Test collations/charsets first, before jumping to live alterations

---
+++ tests/42-charsets-and-collations.sql
@@ -0,0 +1,40 @@
+--
+-- PURPOSE
+-- =======
+-- This tests the effects of default charsets and collations, or overriding
+-- them at the table or column levels.
+--
+
+/*
+
+  t1:
+      All defaults
+
+  t2-t16:
+      Test overrides at the table level. Here the examples are constructed to
+      test:
+        - No explicit collations
+        - Explicit overrides of what is the default anyway
+        - Explicit overrides to something that isn't the default
+
+ */
+
+CREATE TABLE t01 (a VARCHAR(12));
+
+CREATE TABLE t02 (a VARCHAR(12)) CHARACTER SET=latin1;
+CREATE TABLE t03 (a VARCHAR(12)) COLLATE=latin1_swedish_ci;
+CREATE TABLE t04 (a VARCHAR(12)) COLLATE=latin1_spanish_ci;
+CREATE TABLE t05 (a VARCHAR(12)) CHARACTER SET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE t06 (a VARCHAR(12)) CHARACTER SET=latin1 COLLATE=latin1_spanish_ci;
+
+CREATE TABLE t07 (a VARCHAR(12)) CHARACTER SET utf8;
+CREATE TABLE t08 (a VARCHAR(12)) COLLATE utf8_general_ci;
+CREATE TABLE t09 (a VARCHAR(12)) COLLATE utf8_unicode_ci;
+CREATE TABLE t10 (a VARCHAR(12)) CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE TABLE t11 (a VARCHAR(12)) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE t12 (a VARCHAR(12)) CHARACTER SET=utf8mb4;
+CREATE TABLE t13 (a VARCHAR(12)) COLLATE=utf8mb4_general_ci;
+CREATE TABLE t14 (a VARCHAR(12)) COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE t15 (a VARCHAR(12)) CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE t16 (a VARCHAR(12)) CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
