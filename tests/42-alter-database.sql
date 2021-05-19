4b24f3496f2fca48ae60ea9b33a29ac2b8f2c405
Vincent Driessen <me@nvie.com>
2021-05-19 09:22:28 +0200

    Increase test complexity

--- tests/42-alter-database.sql
+++ tests/42-alter-database.sql
@@ -1,7 +1,25 @@
+-- Just to create _some_ output. The point is that the above query is parsed
+-- and accepted.
+CREATE TABLE t1 (
+  id INT,
+  a VARCHAR(12),
+  b VARCHAR(12) CHARACTER SET utf8 COLLATE utf8_general_ci,
+  c VARCHAR(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  d TEXT,
+  e TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci
+);
+
 ALTER DATABASE __dbname__
   DEFAULT CHARACTER SET utf8mb4
   DEFAULT COLLATE utf8mb4_unicode_ci;
 
 -- Just to create _some_ output. The point is that the above query is parsed
 -- and accepted.
-CREATE TABLE t1 (id INT);
+CREATE TABLE t2 (
+  id INT,
+  a VARCHAR(12),
+  b VARCHAR(12) CHARACTER SET utf8 COLLATE utf8_general_ci,
+  c VARCHAR(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  d TEXT,
+  e TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci
+);
