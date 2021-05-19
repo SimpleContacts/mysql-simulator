d044212ddef59e0e917595b2c4bd0a6813c4517e
Vincent Driessen <me@nvie.com>
2021-05-19 17:29:11 +0200

    Add more tests

--- tests/42-charsets-and-collations.sql
+++ tests/42-charsets-and-collations.sql
@@ -17,6 +17,13 @@
         - Explicit overrides of what is the default anyway
         - Explicit overrides to something that isn't the default
 
+  t17-t31:
+      Test overrides at the column level. Here the examples are constructed to
+      test:
+        - No explicit collations
+        - Explicit overrides of what is the default anyway
+        - Explicit overrides to something that isn't the default
+
  */
 
 CREATE TABLE t01 (a VARCHAR(12));
@@ -38,3 +45,23 @@ CREATE TABLE t13 (a VARCHAR(12)) COLLATE=utf8mb4_general_ci;
 CREATE TABLE t14 (a VARCHAR(12)) COLLATE=utf8mb4_unicode_ci;
 CREATE TABLE t15 (a VARCHAR(12)) CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE t16 (a VARCHAR(12)) CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+CREATE TABLE t17 (a VARCHAR(12) CHARACTER SET latin1);
+CREATE TABLE t18 (a VARCHAR(12) COLLATE latin1_swedish_ci);
+CREATE TABLE t19 (a VARCHAR(12) COLLATE latin1_spanish_ci);
+CREATE TABLE t20 (a VARCHAR(12) CHARACTER SET latin1 COLLATE latin1_swedish_ci);
+CREATE TABLE t21 (a VARCHAR(12) CHARACTER SET latin1 COLLATE latin1_spanish_ci);
+
+CREATE TABLE t22 (a VARCHAR(12) CHARACTER SET utf8);
+CREATE TABLE t23 (a VARCHAR(12) COLLATE utf8_general_ci);
+CREATE TABLE t24 (a VARCHAR(12) COLLATE utf8_unicode_ci);
+CREATE TABLE t25 (a VARCHAR(12) CHARACTER SET utf8 COLLATE utf8_general_ci);
+CREATE TABLE t26 (a VARCHAR(12) CHARACTER SET utf8 COLLATE utf8_unicode_ci);
+
+CREATE TABLE t27 (a VARCHAR(12) CHARACTER SET utf8mb4);
+CREATE TABLE t28 (a VARCHAR(12) COLLATE utf8mb4_general_ci);
+CREATE TABLE t29 (a VARCHAR(12) COLLATE utf8mb4_unicode_ci);
+CREATE TABLE t30 (a VARCHAR(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci);
+CREATE TABLE t31 (a VARCHAR(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci);
