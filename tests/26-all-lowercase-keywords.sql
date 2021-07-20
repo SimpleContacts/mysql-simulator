809bad0ba52d2f57e331ae2c52bd3301409e8539
Vincent Driessen <me@nvie.com>
2021-07-20 14:35:57 +0200

    Make test more advanced

--- tests/26-all-lowercase-keywords.sql
+++ tests/26-all-lowercase-keywords.sql
@@ -4,7 +4,10 @@ create table aaa (id int primary key);
 create table bbb (id int primary key);
 alter table bbb
   add column a int null,
-  add constraint foreign key(a) references aaa(id);
+  add foreign key(a) references aaa(id),
+  add constraint foreign key(a) references aaa(id),
   --             ^^^^^^^
-  --             This should not be interpreted as an identifier, but as
-  --             a keyword
+  --             This should NOT be interpreted as an identifier, but as keyword
+  add constraint xxxxxxx foreign key(a) references aaa(id);
+  --             ^^^^^^^
+  --             This SHOULD get interpreted as an identifier, not as a keyword
